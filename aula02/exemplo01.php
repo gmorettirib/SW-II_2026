422da9d179f57d158d064b41e7f6c4022bf5a4dc
Guilherme Moretti <160153078+gmorettirib@users.noreply.github.com>
2026-02-19 10:29:25 -0300

    Add files via upload

--- aula02/exemplo01.php
+++ aula02/exemplo01.php
@@ -1,13 +1,14 @@
 <?php 
-   
+    $nota = 7;
+    
     echo "<br>";
-    for($i = 1; $i <= 8; $i++){
-        if($i % 2 == 1 ){
-            $texto = "aprovado";
+    for($i = 1; $i <= 5; $i++){
+        if($i % 2 == 0 ){
+            $resto = "aprovado";
         } else{
-            $texto = "reprovado";
+            $resto = "reprovado";
         }
-        echo "Numero: " . $i . " ==> ". $texto ."<br>";  
+        echo "Numero: " . $i ."=====>". $resto . "<br>";  
     }
     
 ?>
